66bccb4dd0220a5fd24d0dc435d23aa5324e6676
anst-foto <starininandrey@gmail.com>
2017-10-23 18:02:33 +0300

    j,yjdktybt

--- ps2exe-gui/Build.ps1
+++ ps2exe-gui/Build.ps1
@@ -4,12 +4,12 @@
 # Starinin Andrey (AnSt), 2017
 # Адаптация под свои проекты
 
-$SCRIPTPATH = Split-Path $SCRIPT:MyInvocation.MyCommand.Path -parent
+$SCRIPTPATH = Split-Path $SCRIPT:MyInvocation.MyCommand.Path -Parent
 ls "$SCRIPTPATH\Build\*.ps1" | %{
-	."$SCRIPTPATH\ps2exe.ps1" "$($_.Fullname)" "$($_.Fullname -replace '.ps1','.exe')" -verbose -iconFile "D:\Programming\PowerShell\Broom\Broom.ico"
+	."$SCRIPTPATH\ps2exe.ps1" "$($_.Fullname)" "$($_.Fullname -replace '.ps1','.exe')" -Verbose -IconFile "$SCRIPTPATH\Build\Broom.ico"
 }
 
-Remove-Item "$SCRIPTPATH\Build\Progress.exe*"
-Remove-Item "$SCRIPTPATH\Build\ScreenBuffer-GUI.exe*"
+Remove-Item -Path "$SCRIPTPATH\Build\Progress.exe*" -Force -ErrorAction SilentlyContinue -Verbose
+Remove-Item -Path "$SCRIPTPATH\Build\ScreenBuffer-GUI.exe*" -Force -ErrorAction SilentlyContinue -Verbose
 
 Read-Host "Press enter to exit"
