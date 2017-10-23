2e376b08947cf9a829192617bf0ae66b5b2cd867
Starinin Andrey <starininandrey@gmail.com>
2017-10-23 18:03:33 +0300

    Update PublishPowerShellGallery.ps1

--- PublishPowerShellGallery.ps1
+++ PublishPowerShellGallery.ps1
@@ -1,5 +1,3 @@
 ﻿Test-ScriptFileInfo -Path d:\Programming\PowerShell\Broom\Broom.ps1
-Test-ScriptFileInfo -Path d:\Programming\PowerShell\Broom\Broom_Module.psm1
 
 Publish-Script -Path D:\Programming\PowerShell\Broom\Broom.ps1 -NuGetApiKey 514c48fc-74d5-4243-96d9-baad4efc631c
-Publish-Script -Path D:\Programming\PowerShell\Broom\Broom_Module.psm1 -NuGetApiKey 514c48fc-74d5-4243-96d9-baad4efc631c
