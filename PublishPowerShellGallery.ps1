20da0e795f180d1e06e10dabfb22f84d36f67aef
AnSt <starinin-andrey@ya.ru>
2018-09-06 15:59:41 +0300

    Небольшие правки

--- PublishPowerShellGallery.ps1
+++ PublishPowerShellGallery.ps1
@@ -1,3 +1,3 @@
-﻿Test-ScriptFileInfo -Path d:\Programming\PowerShell\Broom\Broom.ps1
+﻿Test-ScriptFileInfo -Path D:\Programming\Broom\PowerShell\Broom\Broom.ps1
 
-Publish-Script -Path D:\Programming\PowerShell\Broom\Broom.ps1 -NuGetApiKey 514c48fc-74d5-4243-96d9-baad4efc631c
+Publish-Script -Path D:\Programming\Broom\PowerShell\Broom\Broom.ps1 -NuGetApiKey 514c48fc-74d5-4243-96d9-baad4efc631c
